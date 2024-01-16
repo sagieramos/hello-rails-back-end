68da9a28972901df81e238d3ce09e049d8991687
Stanley Osagie Ramos <sagiecyber@gmail.com>
2024-01-16 21:57:22 +0100

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,31 @@
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+
+greet_data = [
+  { greet: 'Hello!' },
+  { greet: 'Hi there!' },
+  { greet: 'Greetings!' },
+  { greet: 'Good day!' },
+  { greet: 'Welcome!' }
+]
+
+Message.create!(greet_data)
+
+
